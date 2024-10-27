c8de33fd471cee437ef456997441647183b2a8be
hernaldomeneses <root@Matrix.com.br>
2024-10-27 19:42:38 -0400

    start

---
+++ step_01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Environment Setup and Bash scripting
+## Ensure you have kali Linux, as it provide tools and resouces for penetration testing.
+## Update and upgrade your kali packages:
+# linux debian prompt comand
+
+sudo apt update && sudo apt upgrade -y
+#Remember
+# to Make this scripting runing you need 
+# chmod +x step_01.sh
+# and so on..
+# ./step_01.sh
+#bye
+
---
+++ step_02.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For organizational purposes, 
+## create a main directory 
+## where you'll store all your scripts and outputs.
+dirname="Hacker_proffesional"
+mkdir $dirname && cd $dirname
+#bye
